3699cf9e4a498f1e3fd05ca4c5edeae639b06fd1
Kobi Cummings <kobicummings@gmail.com>
2019-10-03 18:27:46 -0700

    added to html pages and friends.js

---
+++ data/friends.js
@@ -0,0 +1,27 @@
+var friends = [
+    {
+        name: "Charlie", 
+        photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+        scores: [5,1,2,3,1,2,5,1,1,1]
+    },
+    {
+        name: "Toki Wartooth", 
+        photo: "https://memegenerator.net/img/images/400x/16538257.jpg",
+        scores: [5,1,2,3,1,4,2,1,4,1]
+    },{
+        name: "", 
+        photo: "",
+        scores: [2,1,5,3,4,2,5,2,1,3]
+    },{
+        name: "", 
+        photo: "",
+        scores: [4,3,2,5,1,1,5,3,2,4]
+    },{
+        name: "", 
+        photo: "",
+        scores: [2,3,5,2,4,3,2,5,2,5]
+    },
+
+
+
+]
